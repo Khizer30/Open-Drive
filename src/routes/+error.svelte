df7973942df752165ad95fecf12148c4557369cf
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2023-01-27 20:46:06 +0500

    v1.0
    *Error Page Added

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<svelte:head>
+  <title> 404 | Open Drive </title>
+  
+  <meta name="keywords" content="Open Drive, 404" />
+</svelte:head>
+
+<div class="container-fluid d-flex flex-column justify-content-center align-items-center mainContainer">
+  <h1 class="errorH1"> 404 </h1>
+  <h6 class="errorH2"> PAGE NOT FOUND </h6>
+</div>
+
+<style>
+.mainContainer 
+{
+  min-height: 90vh ;
+}
+</style>
